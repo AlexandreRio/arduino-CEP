f35d0be79356a60b2afdac9fc71b2e06533ba50f
Alexandre Rio <contact@alexrio.fr>
2016-02-11 18:12:48 +0100

    add code, allow avg() operation on a spatial window

---
+++ fifo.cpp
@@ -0,0 +1,308 @@
+#include "fifo.h"
+
+ComplexEventManager::ComplexEventManager() : mFifo()
+{}
+
+int ComplexEventManager::processEvent()
+{
+
+    int eventCode;
+    int param;
+    int handledCount = 0;
+
+    if ( mFifo.popEvent( &eventCode, &param ) )
+    {
+        handledCount = mListeners.sendEvent( eventCode, param );
+    }
+
+    return handledCount;
+}
+
+
+/************************************************/
+ComplexEventManager::ListenerList::ListenerList() :
+mNumListeners( 0 ), mDefaultCallback( 0 )
+{
+}
+
+int ComplexEventManager::ListenerList::numListeners()
+{
+    return mNumListeners;
+};
+
+int ComplexEventManager::numListeners()
+{
+    return mListeners.numListeners();
+};
+
+boolean ComplexEventManager::ListenerList::addListener( int eventCode, EventListener listener )
+{
+    // Argument check
+    if ( !listener )
+    {
+        return false;
+    }
+
+    // Check for full dispatch table
+    if ( isFull() )
+    {
+        return false;
+    }
+
+    mListeners[ mNumListeners ].callback = listener;
+    mListeners[ mNumListeners ].eventCode = eventCode;
+    mListeners[ mNumListeners ].enabled 	= true;
+    mNumListeners++;
+
+    return true;
+}
+
+
+boolean ComplexEventManager::ListenerList::removeListener( int eventCode, EventListener listener )
+{
+    if ( mNumListeners == 0 )
+    {
+        return false;
+    }
+
+    int k = searchListeners( eventCode, listener );
+    if ( k < 0 )
+    {
+        return false;
+    }
+
+    for ( int i = k; i < mNumListeners - 1; i++ )
+    {
+        mListeners[ i ].callback  = mListeners[ i + 1 ].callback;
+        mListeners[ i ].eventCode = mListeners[ i + 1 ].eventCode;
+        mListeners[ i ].enabled   = mListeners[ i + 1 ].enabled;
+    }
+    mNumListeners--;
+
+    return true;
+}
+
+
+int ComplexEventManager::ListenerList::removeListener( EventListener listener )
+{
+    if ( mNumListeners == 0 )
+    {
+        return 0;
+    }
+
+    int removed = 0;
+    int k;
+    while ((k = searchListeners( listener )) >= 0 )
+    {
+        for ( int i = k; i < mNumListeners - 1; i++ )
+        {
+            mListeners[ i ].callback  = mListeners[ i + 1 ].callback;
+            mListeners[ i ].eventCode = mListeners[ i + 1 ].eventCode;
+            mListeners[ i ].enabled   = mListeners[ i + 1 ].enabled;
+        }
+        mNumListeners--;
+        removed++;
+   }
+
+    return removed;
+}
+
+
+boolean ComplexEventManager::ListenerList::enableListener( int eventCode, EventListener listener, boolean enable )
+{
+    if ( mNumListeners == 0 )
+    {
+        return false;
+    }
+
+    int k = searchListeners( eventCode, listener );
+    if ( k < 0 )
+    {
+        return false;
+    }
+
+    mListeners[ k ].enabled = enable;
+
+    return true;
+}
+
+
+boolean ComplexEventManager::ListenerList::isListenerEnabled( int eventCode, EventListener listener )
+{
+    if ( mNumListeners == 0 )
+    {
+        return false;
+    }
+
+    int k = searchListeners( eventCode, listener );
+    if ( k < 0 )
+    {
+        return false;
+    }
+
+    return mListeners[ k ].enabled;
+}
+
+
+int ComplexEventManager::ListenerList::sendEvent( int eventCode, int param )
+{
+    int handlerCount = 0;
+    for ( int i = 0; i < mNumListeners; i++ )
+    {
+        if ( ( mListeners[ i ].callback != 0 ) && ( mListeners[ i ].eventCode == eventCode ) && mListeners[ i ].enabled )
+        {
+            handlerCount++;
+            (*mListeners[ i ].callback)( eventCode, param );
+        }
+    }
+
+    if ( !handlerCount )
+    {
+        if ( ( mDefaultCallback != 0 ) && mDefaultCallbackEnabled )
+        {
+            handlerCount++;
+            (*mDefaultCallback)( eventCode, param );
+
+        }
+
+    }
+
+    return handlerCount;
+}
+
+
+boolean ComplexEventManager::ListenerList::setDefaultListener( EventListener listener )
+{
+    if ( listener == 0 )
+    {
+        return false;
+    }
+
+    mDefaultCallback = listener;
+    mDefaultCallbackEnabled = true;
+    return true;
+}
+
+
+void ComplexEventManager::ListenerList::removeDefaultListener()
+{
+    mDefaultCallback = 0;
+    mDefaultCallbackEnabled = false;
+}
+
+
+void ComplexEventManager::ListenerList::enableDefaultListener( boolean enable )
+{
+    mDefaultCallbackEnabled = enable;
+}
+
+
+int ComplexEventManager::ListenerList::searchListeners( int eventCode, EventListener listener )
+{
+
+    for ( int i = 0; i < mNumListeners; i++ )
+    {
+
+
+        if ( ( mListeners[i].eventCode == eventCode ) && ( mListeners[i].callback == listener ) )
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+int ComplexEventManager::ListenerList::searchListeners( EventListener listener )
+{
+    for ( int i = 0; i < mNumListeners; i++ )
+    {
+        if ( mListeners[i].callback == listener )
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+int ComplexEventManager::ListenerList::searchEventCode( int eventCode )
+{
+    for ( int i = 0; i < mNumListeners; i++ )
+    {
+        if ( mListeners[i].eventCode == eventCode )
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+/*****************************************************/
+// FIFO part
+/*****************************************************/
+
+ComplexEventManager::Fifo::Fifo()
+{}
+
+int ComplexEventManager::Fifo::length()
+{
+  if (fifo_tail >= fifo_head)
+    return fifo_tail - fifo_head;
+  return fifo_tail + FIFO_SIZE - fifo_head;
+}
+
+boolean ComplexEventManager::Fifo::isFull()
+{
+  return fifo_head == ((fifo_tail + 1) % FIFO_SIZE);
+}
+
+
+boolean ComplexEventManager::Fifo::isEmpty()
+{
+  return fifo_head == fifo_tail;
+}
+
+boolean ComplexEventManager::Fifo::queueEvent(int eventCode, int eventParam)
+{
+  int new_tail = (fifo_tail + 1) % FIFO_SIZE;
+  if (new_tail == fifo_head)
+  {
+    fifo_head = (fifo_head + 1) % FIFO_SIZE;
+  }
+
+  fifo[fifo_tail].code = eventCode;
+  fifo[fifo_tail].param = eventParam;
+  fifo_tail = new_tail;
+  return 1;
+}
+
+boolean ComplexEventManager::Fifo::popEvent(int* eventCode, int* eventParam)
+{
+  if (isEmpty())
+    return false;
+
+  *eventCode = fifo[fifo_head].code;
+  *eventParam = fifo[fifo_head].param;
+  fifo_head = (fifo_head + 1)%FIFO_SIZE;
+  return true;
+}
+
+int ComplexEventManager::Fifo::avg()
+{
+  //Serial.println("------------");
+  //Serial.println(fifo_tail);
+  //Serial.println(fifo_head);
+  unsigned long sum = 0;
+  int i = fifo_head;
+  while (i != fifo_tail)
+  {
+    sum += fifo[i].param;
+    i = (i +1)%FIFO_SIZE;
+  }
+  //String s = "sum ";
+  //Serial.println(s + sum);
+  return sum/length();
+}
---
+++ fifo.h
@@ -0,0 +1,281 @@
+#ifndef Fifo_h
+#define Fifo_h
+
+#include <Arduino.h>
+
+#define FIFO_SIZE 12
+#define EVENTMANAGER_LISTENER_LIST_SIZE 8
+
+class ComplexEventManager
+{
+
+  public:
+    typedef void ( *EventListener )( int eventCode, int eventParam );
+
+    enum EventType
+    {
+      kEventNone = 200,
+      kEventSensor1,
+      kEventSensor2
+    };
+
+    ComplexEventManager();
+
+    // Add a listener
+    // Returns true if the listener is successfully installed, false otherwise (e.g. the dispatch table is full)
+    boolean addListener( int eventCode, EventListener listener );
+
+    // Remove (event, listener) pair (all occurrences)
+    // Other listeners with the same function or event code will not be affected
+    boolean removeListener( int eventCode, EventListener listener );
+
+    // Remove all occurrances of a listener
+    // Removes this listener regardless of the event code; returns number removed
+    // Useful when one listener handles many different events
+    int removeListener( EventListener listener );
+
+    // Enable or disable a listener
+    // Return true if the listener was successfully enabled or disabled, false if the listener was not found
+    boolean enableListener( int eventCode, EventListener listener, boolean enable );
+
+    // Returns the current enabled/disabled state of the (eventCode, listener) combo
+    boolean isListenerEnabled( int eventCode, EventListener listener );
+
+    // The default listener is a callback function that is called when an event with no listener is processed
+    // These functions set, clear, and enable/disable the default listener
+    boolean setDefaultListener( EventListener listener );
+    void removeDefaultListener();
+    void enableDefaultListener( boolean enable );
+
+    // Is the ListenerList empty?
+    boolean isListenerListEmpty();
+
+    // Is the ListenerList full?
+    boolean isListenerListFull();
+
+    int numListeners();
+
+    // Returns true if no events are in the queue
+    boolean isEventQueueEmpty();
+
+    // Returns true if no more events can be inserted into the queue
+    boolean isEventQueueFull();
+
+    // Actual number of events in queue
+    int getNumEventsInQueue();
+
+
+    // tries to insert an event into the queue;
+    // returns true if successful, false if the
+    // queue if full and the event cannot be inserted
+    boolean queueEvent( int eventCode, int eventParam);
+
+    // this must be called regularly (usually by calling it inside the loop() function)
+    int processEvent();
+
+    // this function can be called to process ALL events in the queue
+    // WARNING:  if interrupts are adding events as fast as they are being processed
+    // this function might never return.  YOU HAVE BEEN WARNED.
+    int processAllEvents();
+
+    int avg();
+
+  private:
+    class Fifo
+    {
+      public:
+        Fifo();
+
+        int length();
+        int available();
+        boolean isEmpty();
+        boolean isFull();
+
+        int avg();
+
+        boolean queueEvent(int eventCode, int eventParam);
+        boolean popEvent(int* eventCode, int* eventParam);
+
+      private:
+        struct EventElement
+        {
+          int code;
+          int param;
+        };
+
+        EventElement fifo[FIFO_SIZE];
+        int fifo_head = 0;
+        int fifo_tail = 0;
+    };
+
+    // ListenerList class used internally by EventManager
+    class ListenerList
+    {
+
+      public:
+
+        // Create an event manager
+        ListenerList();
+
+        // Add a listener
+        // Returns true if the listener is successfully installed, false otherwise (e.g. the dispatch table is full)
+        boolean addListener( int eventCode, EventListener listener );
+
+        // Remove event listener pair (all occurrences)
+        // Other listeners with the same function or eventCode will not be affected
+        boolean removeListener( int eventCode, EventListener listener );
+
+        // Remove all occurrances of a listener
+        // Removes this listener regardless of the eventCode; returns number removed
+        int removeListener( EventListener listener );
+
+        // Enable or disable a listener
+        // Return true if the listener was successfully enabled or disabled, false if the listener was not found
+        boolean enableListener( int eventCode, EventListener listener, boolean enable );
+
+        boolean isListenerEnabled( int eventCode, EventListener listener );
+
+        // The default listener is a callback function that is called when an event with no listener is processed
+        boolean setDefaultListener( EventListener listener );
+        void removeDefaultListener();
+        void enableDefaultListener( boolean enable );
+
+        // Is the ListenerList empty?
+        boolean isEmpty();
+
+        // Is the ListenerList full?
+        boolean isFull();
+
+        // Send an event to the listeners; returns number of listeners that handled the event
+        int sendEvent( int eventCode, int param );
+
+        int numListeners();
+
+      private:
+
+        // Maximum number of event/callback entries
+        // Can be changed to save memory or allow more events to be dispatched
+        static const int kMaxListeners = EVENTMANAGER_LISTENER_LIST_SIZE;
+
+        // Actual number of event listeners
+        int mNumListeners;
+
+        // Listener structure and corresponding array
+        struct ListenerItem
+        {
+          EventListener callback;// The listener function
+          int eventCode;// The event code
+          boolean enabled;// Each listener can be enabled or disabled
+        };
+        ListenerItem mListeners[ kMaxListeners ];
+
+        // Callback function to be called for event types which have no listener
+        EventListener mDefaultCallback;
+
+        // Once set, the default callback function can be enabled or disabled
+        boolean mDefaultCallbackEnabled;
+
+        // get the current number of entries in the dispatch table
+        int getNumEntries();
+
+        // returns the array index of the specified listener or -1 if no such event/function couple is found
+        int searchListeners( int eventCode, EventListener listener);
+        int searchListeners( EventListener listener );
+        int searchEventCode( int eventCode );
+
+    };
+
+    Fifo mFifo;
+    ListenerList mListeners;
+};
+
+inline int ComplexEventManager::avg()
+{
+  return mFifo.avg();
+}
+
+inline boolean ComplexEventManager::addListener( int eventCode, EventListener listener )
+{
+    return mListeners.addListener( eventCode, listener );
+}
+
+inline boolean ComplexEventManager::removeListener( int eventCode, EventListener listener )
+{
+    return mListeners.removeListener( eventCode, listener );
+}
+
+inline int ComplexEventManager::removeListener( EventListener listener )
+{
+    return mListeners.removeListener( listener );
+}
+
+inline boolean ComplexEventManager::enableListener( int eventCode, EventListener listener, boolean enable )
+{
+    return mListeners.enableListener( eventCode, listener, enable );
+}
+
+inline boolean ComplexEventManager::isListenerEnabled( int eventCode, EventListener listener )
+{
+    return mListeners.isListenerEnabled( eventCode, listener );
+}
+
+inline boolean ComplexEventManager::setDefaultListener( EventListener listener )
+{
+    return mListeners.setDefaultListener( listener );
+}
+
+inline void ComplexEventManager::removeDefaultListener()
+{
+    mListeners.removeDefaultListener();
+}
+
+inline void ComplexEventManager::enableDefaultListener( boolean enable )
+{
+    mListeners.enableDefaultListener( enable );
+}
+
+inline boolean ComplexEventManager::isListenerListEmpty()
+{
+    return mListeners.isEmpty();
+}
+
+inline boolean ComplexEventManager::isListenerListFull()
+{
+    return mListeners.isFull();
+}
+
+inline boolean ComplexEventManager::isEventQueueFull()
+{
+  return mFifo.isFull();
+}
+
+inline boolean ComplexEventManager::isEventQueueEmpty()
+{
+  return mFifo.isEmpty();
+}
+
+inline int ComplexEventManager::getNumEventsInQueue()
+{
+  return mFifo.length();
+}
+
+inline boolean ComplexEventManager::queueEvent(int eventCode,int eventParam)
+{
+  return mFifo.queueEvent(eventCode, eventParam);
+}
+
+inline boolean ComplexEventManager::ListenerList::isEmpty()
+{
+    return (mNumListeners == 0);
+}
+
+inline boolean ComplexEventManager::ListenerList::isFull()
+{
+    return (mNumListeners == kMaxListeners);
+}
+
+inline int ComplexEventManager::ListenerList::getNumEntries()
+{
+    return mNumListeners;
+}
+#endif
