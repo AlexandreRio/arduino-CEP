ab9d97b57cc07fdb100a2f3b1211fb5b0868b2f0
Alexandre Rio <contact@alexrio.fr>
2016-03-04 12:35:03 +0100

    fix loop buffer behavior

--- cep.cpp
+++ cep.cpp
@@ -14,7 +14,15 @@ void streamFilteredJoin1_Fifo::checkTrigger()
 {
   if (!m1_isEmpty() && !m2_isEmpty())
   {
-    
+    // pop all the events
+    m1_popEvent();
+    m2_popEvent();
+
+    // May need to surround this with another guard on all
+    // the events. So pop should reconstruct the events.
+
+    // create the output event
+    // See the rest of the generated ThingML code
   }
 }
 
@@ -51,28 +59,57 @@ void streamFilteredJoin1_Fifo::m1_queueEvent(int param)
   if (m1_available() >= M1_ELEMENT_SIZE)
   {
 
-    union param_t {
-      int content;
-      byte buffer[2];
-    } param_un;
-    param_un.content = param;
-    m1_fifo[m1_fifo_tail] = param_un.buffer[1] & 0xFF;
-    m1_fifo[m1_fifo_tail + 1] = param_un.buffer[0] & 0xFF;
-
     union stamp_t {
       unsigned long time;
       byte time_buffer[TIMESTAMP_DATASIZE];
     } stamp;
     stamp.time = millis();
-    m1_fifo[m1_fifo_tail + 2] = stamp.time_buffer[3] & 0xFF;
-    m1_fifo[m1_fifo_tail + 3] = stamp.time_buffer[2] & 0xFF;
-    m1_fifo[m1_fifo_tail + 4] = stamp.time_buffer[1] & 0xFF;
-    m1_fifo[m1_fifo_tail + 5] = stamp.time_buffer[0] & 0xFF;
+    m1_fifo[m1_fifo_tail    ] = stamp.time_buffer[3] & 0xFF;
+    m1_fifo[m1_fifo_tail + 1] = stamp.time_buffer[2] & 0xFF;
+    m1_fifo[m1_fifo_tail + 2] = stamp.time_buffer[1] & 0xFF;
+    m1_fifo[m1_fifo_tail + 3] = stamp.time_buffer[0] & 0xFF;
+
+    union param_t {
+      int content;
+      byte buffer[2];
+    } param_un;
+    param_un.content = param;
+    m1_fifo[m1_fifo_tail + 4] = param_un.buffer[1] & 0xFF;
+    m1_fifo[m1_fifo_tail + 5] = param_un.buffer[0] & 0xFF;
 
     m1_fifo_tail = (m1_fifo_tail + M1_ELEMENT_SIZE) % M1_FIFO_SIZE;
   }
 
   checkTrigger();
+}
+
+void streamFilteredJoin1_Fifo::m1_popEvent()
+{
+  // For optimize purposes the guard should be removed
+  // because we manage when the method is called
+  if (!m1_isEmpty())
+  {
+
+    union stamp_t {
+      unsigned long time;
+      byte time_buffer[TIMESTAMP_DATASIZE];
+    } stamp;
+
+    stamp.time_buffer[3] = m1_fifo[m1_fifo_head];
+    stamp.time_buffer[2] = m1_fifo[(m1_fifo_head + 1) % M1_FIFO_SIZE];
+    stamp.time_buffer[1] = m1_fifo[(m1_fifo_head + 2) % M1_FIFO_SIZE];
+    stamp.time_buffer[0] = m1_fifo[(m1_fifo_head + 3) % M1_FIFO_SIZE];
+
+    union param_t {
+      int content;
+      byte buffer[2];
+    } param;
+    param.buffer[1] = m1_fifo[(m1_fifo_head + 4) % M1_FIFO_SIZE];
+    param.buffer[0] = m1_fifo[(m1_fifo_head + 5) % M1_FIFO_SIZE];
+
+    m1_fifo_head = (m1_fifo_head + M1_ELEMENT_SIZE) % M1_FIFO_SIZE;
+  }
+
 }
 
 /*****************************************************/
@@ -114,9 +151,9 @@ void streamFilteredJoin1_Fifo::m2_queueEvent()
     } stamp;
     stamp.time = millis();
     m2_fifo[m2_fifo_tail    ] = stamp.time_buffer[3] & 0xFF;
-    m2_fifo[m2_fifo_tail + 1] = stamp.time_buffer[2] & 0xFF;
-    m2_fifo[m2_fifo_tail + 2] = stamp.time_buffer[1] & 0xFF;
-    m2_fifo[m2_fifo_tail + 3] = stamp.time_buffer[0] & 0xFF;
+    m2_fifo[(m2_fifo_tail + 1) % M2_FIFO_SIZE] = stamp.time_buffer[2] & 0xFF;
+    m2_fifo[(m2_fifo_tail + 2) % M2_FIFO_SIZE] = stamp.time_buffer[1] & 0xFF;
+    m2_fifo[(m2_fifo_tail + 3) % M2_FIFO_SIZE] = stamp.time_buffer[0] & 0xFF;
 
     m2_fifo_tail = (m2_fifo_tail + M2_ELEMENT_SIZE) % M2_FIFO_SIZE;
   }
@@ -124,6 +161,24 @@ void streamFilteredJoin1_Fifo::m2_queueEvent()
   checkTrigger();
 }
 
+void streamFilteredJoin1_Fifo::m2_popEvent()
+{
+  if (!m2_isEmpty())
+  {
+
+    union stamp_t {
+      unsigned long time;
+      byte time_buffer[TIMESTAMP_DATASIZE];
+    } stamp;
+
+    stamp.time_buffer[3] = m2_fifo[m2_fifo_head];
+    stamp.time_buffer[2] = m2_fifo[(m2_fifo_head + 1) % M2_FIFO_SIZE];
+    stamp.time_buffer[1] = m2_fifo[(m2_fifo_head + 2) % M2_FIFO_SIZE];
+    stamp.time_buffer[0] = m2_fifo[(m2_fifo_head + 3) % M2_FIFO_SIZE];
+
+    m2_fifo_head = (m2_fifo_head + M2_ELEMENT_SIZE) % M2_FIFO_SIZE;
+  }
+}
 
 void streamFilteredJoin1_Fifo::dump()
 {
@@ -140,10 +195,10 @@ void streamFilteredJoin1_Fifo::dump()
 //    //if (tmpEventElmt.code == 0) {
 //    //  byte c[] = (*tmpEventElmt.content);
 //    //}
-//    //Serial.print(lb + tmpEventElmt.code + co + c[0] + rb);
+//    ////Serial.print(lb + tmpEventElmt.code + co + c[0] + rb);
 //
 //    i = (i +1)%FIFO_SIZE;
 //  }
-//  Serial.println();
+//  //Serial.println();
 //
 }
